02f5db6991d8a3db5ecec1891a8013f58abf7dd9
prettydude <Coolboy.mc1@gmail.com>
2021-02-13 14:43:59 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const path = require('path');
+
+const parser = require("./parser.js");
+
+const dir = "data";
+const exportPath = path.join(__dirname, dir);
+const timetableDir = "timetables";
+
+doParsing();
+
+async function doParsing() {
+	getInstitutes().then(inst => {
+		writeFile(path.join(exportPath, "institutes.json"), JSON.stringify(inst, null, 4));
+	}).catch(err => {
+		console.log("Got error while downloading institutes:", err);
+		process.exit(1)
+	})
+	
+
+	let groups = await getGroups().catch(err => {
+		console.log("Got error while downloading groups:", err);
+		process.exit(1)
+	});
+	groups = groups.map(el => el.trim());
+	writeFile(path.join(exportPath, "groups.json"), JSON.stringify(groups, null, 4));
+
+	let groups2 = groups.splice(0, groups.length/2);
+
+	await Promise.all([fetchTimetables(groups), fetchTimetables(groups2)]); // I/O works in async mode
+	
+	console.log("Done!");
+}
+
+async function fetchTimetables(groups) {
+	let failed = [];
+	for(let group of groups) {
+		await getTimetable(group).then(timetable => {
+			writeFile(path.join(exportPath, timetableDir, group+".json"), JSON.stringify(timetable, null, 4));
+		}).catch(err => {
+			console.log(`Failed to get timetable for ${group}. Retrying later`);
+			failed.push(group);
+		})
+	}
+
+	for(let group of failed) {
+		await getTimetable(group).then(timetable => {
+			writeFile(path.join(exportPath, timetableDir, group+".json"), JSON.stringify(timetable, null, 4));
+		}).catch(err => {
+			console.log(`Failed again while trying to get timetable for ${group}. Error:`, err);
+		})
+	}
+}
+
+async function getTimetable(group) {
+	console.log("Downloading group "+ group + " timetable")
+	return parser.getTimetable(group).catch(err => {
+		return parser.getTimetable(group).catch(err => { // try again
+			return parser.getTimetable(group); // and again
+		}); 
+	})
+}
+
+async function getGroups(institute) {
+	console.log("Downloading groups "+ (institute || ""))
+	let groups = await parser.getGroups()
+	groups.sort(localeCompare);
+	return groups;
+}
+
+async function getInstitutes() {
+	console.log("Downloading institutes");
+	let inst = await parser.getInstitutes()
+	inst.sort(localeCompare);
+	return inst;
+}
+
+function writeFile(filePath, contents) {
+	fs.mkdirSync(path.dirname(filePath), { recursive: true });
+
+	fs.writeFile(filePath, contents, err => {
+		if(err) console.log("Error: ", err);
+	});
+}
+
+function localeCompare(a, b) {
+	return a.localeCompare(b);
+}
